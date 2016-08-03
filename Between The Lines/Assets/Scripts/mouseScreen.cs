31eee959e49be498d7007754b5a2c2567b4f049b
Sean Lu <Sean Lu>
2016-08-03 11:23:25 -0400

    no message

--- Between The Lines/Assets/Scripts/mouseScreen.cs
+++ Between The Lines/Assets/Scripts/mouseScreen.cs
@@ -10,6 +10,31 @@ public class mouseScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetAxis("Mouse X") < 0)
+        {
+            //Code for action on mouse moving left
+            transform.Rotate(new Vector3(0, -3, 0));
+        }
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            //Code for action on mouse moving right
+            transform.Rotate(new Vector3(0, 3, 0));
+        }
+
+        if (Input.GetAxis("Mouse Y") < 0)
+        {
+            //Code for action on mouse moving left
+            transform.Rotate(new Vector3(3, 0, 0));
+        }
+        if (Input.GetAxis("Mouse Y") > 0)
+        {
+            //Code for action on mouse moving right
+            transform.Rotate(new Vector3(-3, 0, 0));
+        }
+
+
+
+       
+
+    }
 }
