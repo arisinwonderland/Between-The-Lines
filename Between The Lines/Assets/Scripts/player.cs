80076d613ceb4bedfe0e0afd1bb1348b460d264a
alexloon123 <alexloon123@gmail.com>
2016-08-03 11:18:28 -0400

    tags and objects and jumping oh my

--- Between The Lines/Assets/Scripts/player.cs
+++ Between The Lines/Assets/Scripts/player.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class player : MonoBehaviour {
 
 	public bool grounded = false;
+    public bool isjumping = false;
+    public float jumptimer = 0;
+
 	public int moveSpeed = 10;
 
 	// Use this for initialization
@@ -18,26 +21,36 @@ public class player : MonoBehaviour {
 		} else if (Input.GetKey (KeyCode.D)) {
 			move (Vector3.right);
 		} else if (Input.GetKey (KeyCode.W)) {
-			move (Vector3.up);
+			move (Vector3.forward);
 		} else if (Input.GetKey (KeyCode.S)) {
-			move (Vector3.down);
+			move (Vector3.back);
 		}
 		if (Input.GetKey (KeyCode.Space)) {
-			
+			if(grounded == true && isjumping == false)
+            {
+                grounded = false;
+                isjumping = true;
+            }
 		}
+        if(isjumping == true)
+        {
+            move(Vector3.up);
+            jumptimer += Time.deltaTime;
+            if(jumptimer >= 2)
+            {
+                jumptimer = 0;
+                isjumping = false;
+            }
+        }
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("Floor")) {
 			grounded = true;
+            isjumping = false;
 		}
 	}
 
-	void OnTriggerExit(Collider other) {
-		if (other.gameObject.CompareTag ("Floor")) {
-			grounded = false;
-		}
-	}
 
 	void move(Vector3 direction) {
 		if (grounded) {
