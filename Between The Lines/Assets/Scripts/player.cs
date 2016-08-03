7355b796e65303d3884dee158b3c0a09897e9452
Sean Lu <Sean Lu>
2016-08-03 15:58:51 -0400

    trinna fix garbage code

--- Between The Lines/Assets/Scripts/player.cs
+++ Between The Lines/Assets/Scripts/player.cs
@@ -90,31 +90,14 @@ public class player : MonoBehaviour {
     }
 
 
-<<<<<<< HEAD
+
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Floor"))
         {
             grounded = true;
             isjumping = false;
         }
-=======
-<<<<<<< HEAD
 
-
-
-    void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag ("Floor")) {
-		 	grounded = true;
-=======
-	void OnTriggerEnter(Collider other) {
-		while (other.gameObject.CompareTag ("Floor")) {
-			grounded = true;
->>>>>>> 3d95178ed60d2940ce2c26d42dd148e849a5d696
-            isjumping = false;
- 
-		}
-<<<<<<< HEAD
->>>>>>> origin/master
         else if (other.gameObject.CompareTag("redfloor"))
         {
             grounded = true;
@@ -128,7 +111,7 @@ public class player : MonoBehaviour {
         else grounded = false;
     }
 
-<<<<<<< HEAD
+
     void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Floor"))
@@ -150,11 +133,12 @@ public class player : MonoBehaviour {
 
         }
         else grounded = true;
-=======
+    }
+
     public static void IgnoreLayerCollision(int layer1, int layer2, bool ignore = true)
     {
             
->>>>>>> 3d95178ed60d2940ce2c26d42dd148e849a5d696
+
     }
 
     void move(Vector3 direction) {
