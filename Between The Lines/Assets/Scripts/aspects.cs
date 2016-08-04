048c376fe850aa6a3bb82c26dc7e76574c51b794
alexloon123 <alexloon123@gmail.com>
2016-08-04 12:00:40 -0400

    omg the collorsosr change

--- Between The Lines/Assets/Scripts/aspects.cs
+++ Between The Lines/Assets/Scripts/aspects.cs
@@ -3,14 +3,32 @@ using System.Collections;
 
 public class aspects : MonoBehaviour {
     public bool red;
-
-	// Use this for initialization
-	void Start () {
+    public GameObject player;
+    public GameObject playercam;
+    public GameObject playerfootbox;
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (red)
+            {
+                red = false;
+                player.layer = LayerMask.NameToLayer("bluePlayer");
+                playercam.layer = LayerMask.NameToLayer("bluePlayer");
+                playerfootbox.layer = LayerMask.NameToLayer("bluePlayer");
+            }
+            else if (!red)
+            {
+                red = true;
+                player.layer = LayerMask.NameToLayer("redPlayer");
+                playercam.layer = LayerMask.NameToLayer("redPlayer");
+                playerfootbox.layer = LayerMask.NameToLayer("redPlayer");
+            }
+        }
+    }
 }
