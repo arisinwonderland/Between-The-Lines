18c5558d4cf435677ddc02eb2c31131513f85a60
Sean Lu <Sean Lu>
2016-08-04 11:59:27 -0400

    picking up and putting down. Glitch because of hand & foot trigger

---
+++ Between The Lines/Assets/Scripts/hands.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hands : MonoBehaviour {
+
+    public GameObject obj;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
