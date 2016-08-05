6a7311f19294212cf3f701e69a96ea6321d01f36
alexloon123 <alexloon123@gmail.com>
2016-08-05 11:12:12 -0400

    switch restrictions

--- Between The Lines/Assets/Scripts/aspects.cs
+++ Between The Lines/Assets/Scripts/aspects.cs
@@ -6,33 +6,49 @@ public class aspects : MonoBehaviour {
 	public GameObject player;
 	public GameObject playercam;
 	public GameObject playerfootbox;
+    bool switched;
+    bool grounded;
 
-    
-	// Use this for initialization
-	void Start () {
-        
-	}
+    public player playerscript;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+        playerscript = player.GetComponent<player>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-   
-
-        if (Input.GetKeyDown(KeyCode.Q))
+        grounded = playerscript.grounded;
+        if(grounded == true)
         {
-            if (red)
+            if(switched == true)
             {
-                red = false;
-                player.layer = LayerMask.NameToLayer("bluePlayer");
-                playercam.layer = LayerMask.NameToLayer("bluePlayer");
-                playerfootbox.layer = LayerMask.NameToLayer("bluePlayer");
+                switched = false;
             }
-            else if (!red)
+        }
+
+        if (!switched)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
             {
-                red = true;
-                player.layer = LayerMask.NameToLayer("redPlayer");
-                playercam.layer = LayerMask.NameToLayer("redPlayer");
-                playerfootbox.layer = LayerMask.NameToLayer("redPlayer");
+                if (red)
+                {
+                    red = false;
+                    player.layer = LayerMask.NameToLayer("bluePlayer");
+                    playercam.layer = LayerMask.NameToLayer("bluePlayer");
+                    playerfootbox.layer = LayerMask.NameToLayer("bluePlayer");
+                    switched = true;
+                }
+                else if (!red)
+                {
+                    red = true;
+                    player.layer = LayerMask.NameToLayer("redPlayer");
+                    playercam.layer = LayerMask.NameToLayer("redPlayer");
+                    playerfootbox.layer = LayerMask.NameToLayer("redPlayer");
+                    switched = true;
+                }
             }
         }
     }
