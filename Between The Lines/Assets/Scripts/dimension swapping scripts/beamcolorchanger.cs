d0ca784a409035f81468cbd32ddf55c1c076e902
alexloon123 <alexloon123@gmail.com>
2016-08-15 11:24:49 -0400

    player fix

---
+++ Between The Lines/Assets/Scripts/dimension swapping scripts/beamcolorchanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beamcolorchanger : MonoBehaviour {
+
+    public GameObject gamecontroller;
+    public aspects aspects;
+    public bool red;
+
+    // Use this for initialization
+    void Start () {
+        gamecontroller = GameObject.Find("GameController");
+        aspects = gamecontroller.GetComponent<aspects>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.GetComponent<ParticleAnimator>().colorAnimation.Equals(0);
+
+	}
+}
