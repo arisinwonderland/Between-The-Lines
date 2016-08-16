c520b95ffa6b47b350656d757c1772487a14d5b6
alexloon123 <alexloon123@gmail.com>
2016-08-16 12:24:51 -0400

    abilities

---
+++ Between The Lines/Assets/Scripts/abilities/red2aoe.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class red2aoe : MonoBehaviour {
+
+    public int damage;
+    public float despawn;
+
+    // Use this for initialization
+    void Start () {
+
+        damage = 20;
+        despawn = .5f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        despawn -= Time.deltaTime;
+        if (despawn <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Between The Lines/Assets/Scripts/object scripts/level4rotating.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level4rotating : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(0, 5 * Time.deltaTime, 0);
+	}
+}
