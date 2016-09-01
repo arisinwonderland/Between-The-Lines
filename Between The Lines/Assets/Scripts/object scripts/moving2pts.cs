d7380e9b82859bd4b7e91415daf7747eb368c2ed
alexloon123 <alexloon123@gmail.com>
2016-09-01 15:40:03 -0400

    no message

--- Between The Lines/Assets/Scripts/object scripts/moving2pts.cs
+++ Between The Lines/Assets/Scripts/object scripts/moving2pts.cs
@@ -21,20 +21,22 @@ public class moving2pts : MonoBehaviour
         if (state == 0)
         {
 
-            transform.position = Vector3.MoveTowards(transform.position, pt2.transform.position, 1);
+            transform.position = Vector3.MoveTowards(transform.position, pt2.transform.position, .05f);
 
             if ((transform.position - pt2.transform.position).sqrMagnitude <= 1f)
             {
                 state = 1;
+                transform.eulerAngles = new Vector3(0,180,0);
             }
         }else if (state == 1){
 
-                transform.position = Vector3.MoveTowards(transform.position, pt1.transform.position, 1);
+                transform.position = Vector3.MoveTowards(transform.position, pt1.transform.position, .05f);
 
                 if (transform.position == pt1.transform.position)
                 {
                     state = 0;
-                }
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
             }
         
     }
